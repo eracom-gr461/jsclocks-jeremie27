d39c738677a4c5aafd8f7dcc4ac2360c004b66f6
jeremie27 <31766567+jeremie27@users.noreply.github.com>
2017-09-29 13:11:14 +0200

    rendu

--- js/clocks.js
+++ js/clocks.js
@@ -1,10 +1,13 @@
 
 /*
  * Un petit script mesurant l'écoulement du temps
- * 
+ *
  * Source: https://codepen.io/Tcip/pen/BNKjeN
  * Auteur: Mikael Elmblad, tcip.se
 */
+
+// afficher un cube-montre.
+alert("hello world");
 
 // répéter la fonction metronone une fois par seconde
 
@@ -19,7 +22,7 @@ function metronome() {
   // voir documentation: https://developer.mozilla.org/fr/docs/Web/JavaScript/Reference/Objets_globaux/Date
 
   var d = new Date();
-  
+
   // 2: on affiche l'heure dans notre objet "horloge"
 
   document.getElementById("horloge").innerHTML = d.toLocaleTimeString();
